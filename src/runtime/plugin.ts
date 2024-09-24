52ffb25fd5feb1b23b1a23f7aa6bbc13606f9adc
Benjamin VIELLARD <gbicou@gmail.com>
2024-09-24 13:05:33 +0200

    no auto fortawesome

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,6 +1,7 @@
-import { config } from '@fortawesome/fontawesome-svg-core'
+import { config, noAuto } from '@fortawesome/fontawesome-svg-core'
 import { defineNuxtPlugin } from '#app'
 
+noAuto()
 config.autoAddCss = false
 
 export default defineNuxtPlugin(() => {})
